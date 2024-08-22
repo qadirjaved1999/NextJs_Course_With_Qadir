a37e18998330f77575d48d6b47d4c2c5fd9f1c5d
Qadir Javed <108120593+qadirjaved1999@users.noreply.github.com>
2024-08-22 18:01:17 +0500

    linking and navigation

--- filebasedrouting/app/about/page.js
+++ filebasedrouting/app/about/page.js
@@ -1,7 +1,13 @@
+import Link from 'next/link';
 import React from 'react';
 
 const AboutPage = () => {
-  return <div>About Us</div>;
+  return(
+   <div>
+     <div>About Us</div>
+     <Link href="/">Go to home</Link>
+   </div>
+  );
 };
 
 export default AboutPage;
